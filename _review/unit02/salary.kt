19dd54fc0436976a0ca386327a69f196759fce84
Yahir-21 <157508172+Yahir-21@users.noreply.github.com>
2024-02-13 11:00:19 -0600

    Create salary.kt

---
+++ _review/unit02/salary.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name: String = "Yahir Estrada"
+    var age: Int = 16
+    var annualPay: Double = 999999.99
+
+    println("My name is" + name + ", my age is" + age + "and")
+    println("I hope to earn $ " + annualPay + "per year.")
+    
+//  My name is Yahir Estrada, my age is 16 and  
+//  I hope to earn $999999.99 per year.
+    
+}
