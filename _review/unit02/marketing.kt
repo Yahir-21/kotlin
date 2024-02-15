13000404dee97c7992e0a99f7161a539d28561d1
Yahir-21 <157508172+Yahir-21@users.noreply.github.com>
2024-02-15 10:29:07 -0600

    Create marking.kt

---
+++ _review/unit02/marketing.kt
@@ -0,0 +1,26 @@
+// Name: Yahir Estrada Assignment: 03.2 Advertising Conditional Date: 2/15/2024
+
+fun main() {
+    var friendsCount = 501
+
+    if (friendsCount > 500) {
+        println("Adopt a Dog Today!")
+    } else {
+        println("Buy Cat Food at 20% Off!")
+    }
+
+    if (friendsCount < 500) {
+        println("Adopt a Cat Today!")
+    } else {
+        println("Buy Cat Food at 20% Off!")
+    }
+
+}
+    // if
+    //    "Adopt a dog Today!"
+    // "Buy cat food at 20% off!"
+
+    // A person has so many friends
+    // If the person has more than 500 friends, they like dogs
+    // If the person has less than 500 friends, they like cats
+    // sell them the correct product
