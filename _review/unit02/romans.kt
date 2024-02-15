11fdff58e120b8f199182132fe21a0e6e147e5b6
Yahir-21 <157508172+Yahir-21@users.noreply.github.com>
2024-02-15 11:12:38 -0600

    Create romans.kt

---
+++ _review/unit02/romans.kt
@@ -0,0 +1,41 @@
+// Name: Yahir Estrada Assignment: 04.1 Roman Numerals Date: 2/15/2024
+
+fun main(args: Array<String>) {
+print("Enter a number between 1 and 10:")
+
+    var number = readLine()!!
+
+    var rom = number.toInt()
+
+    if (rom ==1) {
+        println("The roman number is I.")
+    }
+    if (rom ==2) {
+        println("The roman number is II.")
+    }
+    if (rom ==3) {
+        println("The roman number is III.")
+    }
+    if (rom ==4) {
+        println("The roman number is IV.")
+    }
+    if (rom ==5) {
+        println("The roman number is V.")
+    }
+    if (rom ==6) {
+        println("The roman number is VI.")
+    }
+    if (rom ==7) {
+        println("The roman number is VII.")
+    }
+    if (rom ==8) {
+        println("The roman number is VIII.")
+    }
+    if (rom ==9) {
+        println("The roman number is IX.")
+    }
+    if (rom ==10) {
+        println("The roman number is X.")
+    }
+
+}
